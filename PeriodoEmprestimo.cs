5e2bebb301147656d036403246d740c80caddbc7
VITOR JAILSON DA SILVA SANTOS <154376667+Vitorjailson7@users.noreply.github.com>
2025-06-04 21:09:32 -0300

    PeriodoEmprestimo.cs
    - Define propriedades DataEmprestimo e DataDevolucao
    - Implementa validação básica para garantir período válido
    - Facilita controle e monitoramento dos prazos de devolução
    - Base essencial para funcionalidades de empréstimos na biblioteca

---
+++ PeriodoEmprestimo.cs
@@ -0,0 +1,8 @@
+namespace SistemaBiblioteca
+{
+    public struct PeriodoEmprestimo
+    {
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime? DataDevolucao { get; set; }
+    }
+}
